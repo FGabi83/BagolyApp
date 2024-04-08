bca41e1973597bb4b4aeee859bf05fd0f7fc1a66
FGabi83 <77617324+FGabi83@users.noreply.github.com>
2024-04-08 14:36:33 +0200

    footer created

--- public/javascripts/modules/dropdownMenu.js
+++ public/javascripts/modules/dropdownMenu.js
@@ -1,9 +1,10 @@
 function dropdownMenu() {
   const navMenu = document.querySelector(".nav-menu");
   const navWrapper = document.querySelector(".nav--wrapper");
-  const dropdownNavbar = document.querySelector(".nav");
+  
   
   navMenu.addEventListener("click", () => { 
+    console.log("clicked");
     navWrapper.classList.toggle("is-open");
     navMenu.classList.toggle("highlight");
   }); 
